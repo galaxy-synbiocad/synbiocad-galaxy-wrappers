6a6a8ff9dc782e713bbcb7e4e898fab6c00dff20
breakthewall <joan.herisson@univ-evry.fr>
2021-09-13 13:44:26 +0200

    feat(rpRanker): add rpRanker

---
+++ rpRanker/wrap.py
@@ -0,0 +1,40 @@
+from argparse  import ArgumentParser
+from rptools.rplibs import rpPathway
+
+
+def read_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        'pathways',
+        type='str',
+        nargs='+',
+        help='show the version number and exit'
+    )
+    parser.add_argument(
+        '--data_outdir',
+        required=True,
+        type='str',
+        help='show the version number and exit'
+    )
+    return parser.parse_args()
+
+
+def entry_point():
+    args = read_args()
+    pathways = {}
+    for infile in args.pathways:
+        pathways[infile] = rpPathway.from_rpSBML(infile).get_global_score()
+    sorted_pathways = sorted(pathways.items(), key=lambda item: item[1])
+    print(
+        '\n'.join(sorted_pathways)
+    )
+    return sorted_pathways()
+
+
+def _cli():
+    entry_point()
+    return 0
+
+
+if __name__ == '__main__':
+    _cli()
